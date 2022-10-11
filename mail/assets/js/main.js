03ff2747c0ea2547eff0eee1b0f543a1984bea8a
MicheleRossetti02 <michelerossetti07@gmail.com>
2022-10-11 16:36:39 +0200

    mail

--- mail/assets/js/main.js
+++ mail/assets/js/main.js
@@ -1,6 +1,26 @@
 // Mail
 // Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
 
+const mail = [
+    'michelerossetti@gmail.com',
+    'boolean@gmail.com',
+    'mariorossi@gmail.com',
+    'google@gmail.com',
+    'Mandalorean@gmail.com',
+    'serietv@gmail.com'
+]
+
+const userMail = prompt('digita la tua mail');
+
+if(userMail.length== mail.length){
+    mail.push(userMail);
+    console.log('la tua mail è stata aggiunta correttamente');
+} else {
+    console.log('la tua mail non è stata aggiunta correttamente');
+}
+console.log(userMail);
+
+
 
 
 
